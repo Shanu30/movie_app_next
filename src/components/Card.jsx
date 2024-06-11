ec181d0eb5939b8863de7dedfc655f9d75ede61a
kumar shanu <kumarshanu@kumars-MacBook-Air.local>
2024-06-11 11:22:24 +0530

    added styl in cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,20 +5,23 @@ import { FiThumbsUp } from "react-icons/fi";
 
 const Card = ({ result }) => {
   return (
-    <div className="grid sm:grid-cols-2 lg:">
+    <div className="group shadow-md border m-2 p-2 hover:shadow-slate-400 border-slate-400 rounded-lg">
       <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
           }`}
           alt="poster"
-          width={300}
+          width={500}
           height={300}
+          className="group-hover:opacity-70 transition-opacity duration-300"
         ></Image>
-        <div>
-          <p>{result.overview}</p>
-          <h2>{result.title || result.name}</h2>
-          <p>
+        <div className="p-2">
+          <p className="line-clamp-2 text-md">{result.overview}</p>
+          <h2 className="truncate font-bold text-lg">
+            {result.title || result.name}
+          </h2>
+          <p className="flex">
             {result.release_date || result.first_air_date}
             <FiThumbsUp className="h-5 mr-1 ml-3" />
             {result.vote_count}
