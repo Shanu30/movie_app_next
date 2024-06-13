0e3fd08af51431e45468f6a496eb57185154b5dc
kumar shanu <kumarshanu@kumars-MacBook-Air.local>
2024-06-14 01:11:24 +0530

    fixed suspense boundary error

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
@@ -6,16 +6,18 @@ const NavItem = ({ title, apiParam }) => {
   const searchParams = useSearchParams();
   const genre = searchParams.get("genre");
   return (
-    <Link
-      href={`/?genre=${apiParam}`}
-      className={`hover:text-amber-500 font-semibold ${
-        genre === apiParam
-          ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
-          : ""
-      }`}
-    >
-      {title}
-    </Link>
+    <Suspense>
+      <Link
+        href={`/?genre=${apiParam}`}
+        className={`hover:text-amber-500 font-semibold ${
+          genre === apiParam
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+      >
+        {title}
+      </Link>
+    </Suspense>
   );
 };
 
